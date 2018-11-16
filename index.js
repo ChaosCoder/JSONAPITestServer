fbf8d96c34c4b6bb3b8744cb35ca60ff250721e4
Andreas Ganske <a.ganske@chaosspace.de>
2018-11-16 12:51:23 +0100

    Check for 204 or empty body

--- index.js
+++ index.js
@@ -1,13 +1,19 @@
 "use strict";
 
 const express = require('express');
+const bodyParser = require('body-parser');
 const app = express();
 const util = require('util');
 
-app.use(express.json());
+function isEmptyObject(obj) {
+  return Object.keys(obj).length === 0 && obj.constructor === Object;
+}
+
+app.use(bodyParser.raw({type: "*/*"}));
 
 const status = function(req, res, next) {
   res.status(req.get('X-HTTP-STATUS') || 200);
+  res.type('application/json');
   next();
 };
 
@@ -18,7 +24,8 @@ app.get('/get', status, function (req, res) {
 
 app.post('/post', status, function (req, res) {
   console.log('/post called');
-  res.json(req.body);
+  var body = (isEmptyObject(req.body) || res.statusCode == 204) ? null : req.body;
+  res.send(body);
 });
 
 app.get('/redirect', function (req, res) {
@@ -27,7 +34,7 @@ app.get('/redirect', function (req, res) {
 });
 
 app.use(function(error, req, res, next) {
-  res.status(400).json({error: error.message})
+  res.status(400).json({error: error.message});
 });
 
-app.listen(process.env.PORT, () => console.log(`Server listening on port ${process.env.PORT}!`))
+app.listen(process.env.PORT, () => console.log(`Server listening on port ${process.env.PORT}!`));
